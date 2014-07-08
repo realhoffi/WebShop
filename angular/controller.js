dacde155275bbb44703fbe5ba91fb7586aaa5ce1
realhoffi <florian.hoffmann1983@googlemail.com>
2014-07-08 12:02:08 +0200

    initialize

--- angular/controller.js
+++ angular/controller.js
@@ -5,41 +5,66 @@
 var ausgabenmanager = angular.module('appAusgabenmanager', ['ui.bootstrap']);
 
 ausgabenmanager.controller('ausgabenCtrl', function ($scope, $http) {
-    $scope.rootDomain = 'http://info.fhoffma.net/services';
-    $scope.userId = "";
-    $scope.Ausgaben = [];
-    $scope.orderProp = "Name";
+	$scope.rootDomain = 'http://info.fhoffma.net/services';
+	$scope.userId;
+	$scope.Ausgaben = [];
+	$scope.Ausgabenzeitraeume = [];
+	$scope.orderProp = "Name";
 
-    LogIn();
-    $scope.$watch('userId', function () {
-        getAusgaben();
-    });
 
-    function getAusgaben() {
-        $http.get($scope.rootDomain + '/ausgaben/getausgaben?uid=1',
-            {cache: false})
-            .success(function (data) {
-                $scope.Ausgaben = data;
-            })
-            .error(function (data, status, headers) {
-                alert('Fehler beim Datenabruf der Ausgaben... :(');
-            });
-    }
+	LogIn();
+	$scope.updateAusgabe = function (a, b) {
+		for (var i = 0; i < $scope.Ausgabenzeitraeume.length; i++) {
+			if ($scope.Ausgabenzeitraeume[i].ID == a) {
+				b.AusgabezeitraumObject = $scope.Ausgabenzeitraeume[i];
+			}
+		}
+	}
 
-    function LogIn() {
-        var c = app.common.utils.readCookie('userid');
-        if (c && c.length > 0) {
-            $scope.userId = c;
-        } else {
-            $http.post($scope.rootDomain + "/users/SignIn",
-                JSON.stringify({UserId: 'Testadress@gmail.com'}),
-                {dataType: 'json', contentType: "application/json; charset=utf-8"})
-                .success(function (data) {
-                    $scope.userId = data;
-                }).
-                error(function (data, status, headers) {
-                    alert('Fehler beim LogIn... :(');
-                });
-        }
-    }
+	$scope.$watch('userId', function () {
+		if ($scope.userId) {
+			getAusgaben();
+			getAusgabenzeitraeume();
+		}
+	});
+
+	function getAusgaben() {
+		$http.get($scope.rootDomain + '/ausgaben/getausgaben?uid=' + $scope.userId,
+			{cache: false})
+			.success(function (data) {
+				$scope.Ausgaben = data;
+			})
+			.error(function (data, status, headers) {
+				alert('Fehler beim Datenabruf der Ausgaben... :(');
+			});
+	}
+
+	function getAusgabenzeitraeume() {
+		$http.get($scope.rootDomain + '/Ausgabenzeitraum/GetAusgabenzeitraeume?uid=' + $scope.userId,
+			{cache: false})
+			.success(function (data) {
+				$scope.Ausgabenzeitraeume = data;
+			})
+			.error(function (data, status, headers) {
+				alert('Fehler beim Datenabruf der Ausgaben... :(');
+			});
+
+	}
+
+	function LogIn() {
+		var c = app.common.utils.readCookie('userid');
+		if (c && c.length > 0) {
+			$scope.userId = c;
+		} else {
+			$http.post($scope.rootDomain + "/users/SignIn",
+				JSON.stringify({UserId: 'Testadress@gmail.com'}),
+				{dataType: 'json', contentType: "application/json; charset=utf-8"})
+				.success(function (data) {
+					$scope.userId = data;
+				}).
+				error(function (data, status, headers) {
+					alert('Fehler beim LogIn... :(');
+				});
+		}
+	}
 });

77b7499ce96ce725aa6d1b21b49719d0b33a2779
realhoffi <florian.hoffmann1983@googlemail.com>
2014-07-13 11:22:04 +0200

    initialize

--- angular/app.js
+++ angular/app.js
@@ -1,3 +1,15 @@
 /**
  * Created by fhoffma on 07.07.2014.
  */
+if (typeof jQuery === 'undefined') {
+	throw new Error('Bootstrap\'s JavaScript requires jQuery')
+}
++function ($) {
+	'use strict';
+	$(document).on('click.bs.collapse', '[data-toggle=collapse]', function (e) {
+		var b = $(e.currentTarget).find('span.indicator');
+		var c = $(b).toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
+	});
+
+}
+(jQuery);

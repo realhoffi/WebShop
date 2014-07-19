974c62562888072d51c8897fb1e3818fcb3e6422
realhoffi <florian.hoffmann1983@googlemail.com>
2014-07-19 09:46:22 +0200

    initialize

--- angular/app.js
+++ angular/app.js
@@ -3,14 +3,14 @@
  */
 'use strict';
 if (typeof jQuery === 'undefined') {
-    throw new Error('Bootstrap\'s JavaScript requires jQuery')
+	throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 +function ($) {
-    'use strict';
-    $(document).on('click.bs.collapse', '[data-toggle=collapse]', function (e) {
-        var b = $(e.currentTarget).find('span.indicator');
-        var c = $(b).toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
-    });
+	'use strict';
+	$(document).on('click.bs.collapse', '[data-toggle=collapse]', function (e) {
+		var b = $(e.currentTarget).find('span.indicator');
+		var c = $(b).toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
+	});
 //	$("form.input").click(function (e) {
 //		alert(':(');
 //		e.preventDefault();
@@ -19,32 +19,39 @@ if (typeof jQuery === 'undefined') {
 }
 (jQuery)
 
-
 var ausgabenmanager = angular.module('ausgabenmanager', ['ngRoute', 'ui.bootstrap', 'ausgabenmanagerControllers', 'ausgabenmanagerServices']);
 ausgabenmanager.run(function ($rootScope, $log, userService) {
 
-    var maxCountFailCount = 3;
-    $rootScope.needLoginPage = false;
-    $rootScope.isUserLoggedIn = false;
-    $rootScope.userData = null;
-    $rootScope.maxFailCounter = maxCountFailCount;
-    $rootScope.rootDomain = 'http://info.fhoffma.net/services';
-    $log.info("rootScope settings done");
-    $rootScope.resetFailCounter = function () {
-        $rootScope.maxFailCounter = maxCountFailCount;
-    }
-    $rootScope.checkUserData = function () {
-        $log.info('checkUserData is called. FALSE = GOOD, TRUE = BAD  :()');
-        var retVal = (!$rootScope.userData || $rootScope.userData.UserId == 'undefined' ||
-            $rootScope.userData.UserId == undefined || $rootScope.userData.UserId.length == 0);
-        $log.info('checkUserData result: ' + retVal);
-        return retVal;
-    }
-    //StartUp Method to try Login! If not possible, SignIn command windows opens
-    $rootScope.$watch('$viewContentLoaded', function () {
-        $log.info('--WATCH--$viewContentLoaded-- ' + new Date());
-        $log.info('- call tryLogin -');
-        userService.tryLogin();
+	var maxCountFailCount = 3;
+	$rootScope.isAppLoading = true;
+	$rootScope.needLoginPage = false;
+	$rootScope.isUserLoggedIn = false;
+	$rootScope.userData = null;
+	$rootScope.maxFailCounter = maxCountFailCount;
+	$rootScope.rootDomain = 'http://info.fhoffma.net/services';
+	$log.info("rootScope settings done");
+	$rootScope.resetFailCounter = function () {
+		$rootScope.maxFailCounter = maxCountFailCount;
+	}
+	$rootScope.checkUserData = function () {
+		$log.info('checkUserData is called. FALSE = GOOD, TRUE = BAD  :()');
+		var retVal = (!$rootScope.userData || $rootScope.userData.UserId == 'undefined' ||
+			$rootScope.userData.UserId == undefined || $rootScope.userData.UserId.length == 0);
+		$log.info('checkUserData result: ' + retVal);
+		return retVal;
+	}
+	//StartUp Method to try Login! If not possible, SignIn command windows opens
+	$rootScope.$watch('$viewContentLoaded', function () {
+		$log.info('--WATCH--$viewContentLoaded-- ' + new Date());
+		$log.info('- call tryLogin -');
+		userService.tryLogin()
+			.then(function (a) {
+				$rootScope.isAppLoading = false;
+			}, function (error) {
+				alert(error);
+				$rootScope.isAppLoading = false;
+			});
+		//Auskommentiert zum testen :)
 //        if (userService.getUserId) {
 //            $log.info('--WATCH--$viewContentLoaded-- ' + new Date());
 //            userService.tryLogin().then(function (data) {
@@ -60,33 +67,79 @@ ausgabenmanager.run(function ($rootScope, $log, userService) {
 //            $rootScope.userData = null;
 //            return;
 //        }
-    });
+	});
+
+//EVENTS TO SHOW LOADING
+	$rootScope.$on("$routeChangeStart", function (e) {
+		//alert('1');
+		$rootScope.$broadcast("loading-started");
+	});
+	$rootScope.$on("$routeChangeSuccess", function (e) {
+		//alert('2');
+		$rootScope.$broadcast("loading-complete");
+	});
+	$rootScope.$on("$routeChangeError", function (e) {
+		//	alert('3');
+	});
 });
 
 ausgabenmanager.config(['$routeProvider',
-    function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: '../partials/ausgabenOverview.html',
-                controller: 'ausgabenCtrl'
-            }).
-            when('/ausgabenOverview', {
-                templateUrl: '../partials/ausgabenOverview.html',
-                controller: 'ausgabenCtrl'
-            }).
-            when('/ausgabenBericht', {
-                templateUrl: '../partials/ausgabenBericht.html',
-                controller: 'ausgabenCtrl'
-            }).
-            when('/favoritenOverview', {
-                templateUrl: '../partials/favoritenOverview.html',
-                controller: 'favoriteCtrl'
-            }).
-            when('/fileOverview', {
-                templateUrl: '../partials/fileOverview.html',
-                controller: 'fileCtrl'
-            }).
-            otherwise({
-                redirectTo: '/angulartest.html'
-            });
-    }]);
+	function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: '../partials/ausgabenOverview.html',
+				controller: 'ausgabenCtrl'
+			}).
+			when('/ausgabenOverview', {
+				templateUrl: '../partials/ausgabenOverview.html',
+				controller: 'ausgabenCtrl'
+			}).
+			when('/ausgabenBericht', {
+				templateUrl: '../partials/ausgabenBericht.html',
+				controller: 'ausgabenCtrl'
+			}).
+			when('/favoritenOverview', {
+				templateUrl: '../partials/favoritenOverview.html',
+				controller: 'favoriteCtrl'
+			}).
+			when('/fileOverview', {
+				templateUrl: '../partials/fileOverview.html',
+				controller: 'fileCtrl'
+			}).
+			otherwise({
+				redirectTo: '/angulartest.html'
+			});
+	}]);
+ausgabenmanager.config(function ($httpProvider) {
+	$httpProvider.interceptors.push(function ($q, $rootScope) {
+		return {
+			'request': function (config) {
+				$rootScope.$broadcast('loading-started');
+				return config || $q.when(config);
+			},
+			'response': function (response) {
+				$rootScope.$broadcast('loading-complete');
+				return response || $q.when(response);
+			}
+		};
+	});
+});
+
+ausgabenmanager.directive("loadingIndicator", function () {
+	return {
+		restrict: "A",
+		template: "<div class='alert alert-info'>Loading...</div>",
+		link: function (scope, element, attrs) {
+			scope.$on("loading-started", function (e) {
+				element.css({"display": ""});
+				//	alert('s');
+			});
+
+			scope.$on("loading-complete", function (e) {
+				//alert('h');
+				element.css({"display": "none"});
+			});
+
+		}
+	};
+});
